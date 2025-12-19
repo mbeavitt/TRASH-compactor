8f64653aeeed4161087ce074e5286d49622fe359
Michael Beavitt <mbeavitt@gmail.com>
2025-12-19 15:43:48 +0000

    Basic summary statistics and input table
    Now the input is in the form of a table with no header, where the first
    column is the id of the run and the second column is the path to the
    file. The program outputs a tsv summary table with the statistics from
    each input file. Currently the statistics are:
        - Number of repeats
        - Number of unique repeats

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import os
 import argparse
 
 parser = argparse.ArgumentParser(
@@ -8,4 +9,35 @@ parser = argparse.ArgumentParser(
                     description='Analyses output of TRASH',
                     epilog='text at bottom of help - todo')
 
-pd.read_csv("")
+parser.add_argument('input_table')
+
+args = parser.parse_args()
+
+'''
+example = args.input[0]
+
+hor_table = pd.read_csv(example)
+print(hor_table.head())
+print(list(hor_table.columns))
+'''
+
+with open("summary.tsv", "w") as file:
+    print("run_id", end='\t', file=file)
+    print("num_seqs", end='\t', file=file)
+    print("num_unique_seqs", end='\n', file=file)
+
+    input_table = pd.read_csv(args.input_table, sep='\t', header=None)
+    file_paths = list(input_table.iloc[:, 1])
+
+    for index, line in input_table.iterrows():
+        run_id = line[0]
+        path = line[1]
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Could not find input TRASH table: {path}")
+
+        hor_table = pd.read_csv(path)
+
+        print(run_id, end='\t', file=file)
+        print(len(hor_table['sequence']), end='\t', file=file)
+        print(len(list(set(hor_table['sequence']))), end='\n', file=file)
+
