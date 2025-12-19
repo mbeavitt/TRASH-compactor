624891c624c377bf082e71fba579197091e0777d
Michael Beavitt <mbeavitt@gmail.com>
2025-12-19 14:44:33 +0000

    added argparse

---
+++ script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='hor-analysis',
+                    description='Analyses output of TRASH',
+                    epilog='text at bottom of help - todo')
+
+pd.read_csv("")
